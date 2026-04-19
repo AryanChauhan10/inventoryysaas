32700481dd6b181051c75404f1ec103c8402d295
AryanChauhan10 <aryanrana1890@gmail.com>
2026-04-19 21:12:02 +0530

    fix middleware

--- inventory_saas/inventory_saas/settings.py
+++ inventory_saas/inventory_saas/settings.py
@@ -123,7 +123,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    ...
+    
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 STATICFILES_DIRS = [
